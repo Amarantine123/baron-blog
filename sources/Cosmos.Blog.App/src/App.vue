f7e17a009852b66344874868c61449b3c7abb693
lishaobo-baron <s.li@reply.com>
2021-12-29 18:03:34 +0800

    initial folder structure

---
+++ sources/Cosmos.Blog.App/src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  ss
+</template>
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  head: {
+       link: [
+             {
+                 rel: "icon",
+                 href: require("./assets/logo.png")
+             },
+          ]
+       }
+}
+</script>
+
+<style>
+
+</style>
