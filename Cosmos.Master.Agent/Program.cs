b63543552faf241230a5ec4e29c2a0c1e7bf990a
lishaobo-baron <s.li@reply.com>
2022-04-08 18:00:20 +0800

    update

--- Cosmos.Master.Agent/Program.cs
+++ Cosmos.Master.Agent/Program.cs
@@ -1,10 +1,10 @@
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
     ApplicationName = typeof(Program).Assembly.FullName,
-    ContentRootPath =Directory.GetCurrentDirectory(),
-    EnvironmentName= Environments.Staging,
-    WebRootPath="customwwwroot"
-}) ;
+    ContentRootPath = Directory.GetCurrentDirectory(),
+    EnvironmentName = Environments.Staging,
+    WebRootPath = "customwwwroot"
+});
 Console.WriteLine($"Application Name: {builder.Environment.ApplicationName}");
 Console.WriteLine($"Environment Name: {builder.Environment.EnvironmentName}");
 Console.WriteLine($"ContentRoot Path: {builder.Environment.ContentRootPath}");
